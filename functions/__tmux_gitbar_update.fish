72d07c41ea1261d29b95b799fadc74cf02978f7e
Martynas Mickevičius <self@2m.lt>
2018-07-23 22:31:23 +0300

    Move function definition to init file

--- functions/__tmux_gitbar_update.fish
+++
@@ -1,3 +0,0 @@
-function __tmux_gitbar_update --on-event fish_postexec --description 'Update tmux gitbar'
-  eval "$PROMPT_COMMAND"
-end
--- init.fish
+++ init.fish
@@ -1,1 +1,3 @@
-__tmux_gitbar_update
+function __tmux_gitbar_update --on-event fish_postexec --description 'Update tmux gitbar'
+  eval "$PROMPT_COMMAND"
+end
